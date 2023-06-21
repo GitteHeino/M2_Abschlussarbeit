2d5161b7e4f55143cbf9a3286161eba8c1f04a25
Andreas Blechschmidt <99222847+GitteHeino@users.noreply.github.com>
2023-06-21 23:46:57 +0200

    update MBP  …20 23 49 00 an Konto gearbeitet  …

--- src/main/kotlin/KONTO/Konto.kt
+++ src/main/kotlin/KONTO/Konto.kt
@@ -1,20 +1,42 @@
 package KONTO
-
-import KONTO.kundenKonto.datum
-import KONTO.kundenKonto.kontostand
+import date
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
 open class Konto(
     var datum: String = "2023-06-23",
-    val kontostand: Double = 16345.92
+    var kontostand: Double = 16345.92
 ) {
-    fun einzahlen(kundenKontoEinzahlung: Double) {
+    /*    fun einzahlen(kundenKontoEinzahlung: Double) {
+        date()
         print("wie viel möchten Sie einzahlen? (Betrag in € z.B. 100.0):  ")
         var einzahlung = readln()
-        kundenKonto
-        TODO("Not yet implemented")
+        kontostand = kontostand + einzahlung
+
+    }
+}*/
+
+
+    fun einzahlen(einzahlung: Double) {
+        date()
+        readln()
+        kontostand = kontostand + einzahlung
+    }
+
+    fun shopkonto() {
+        println("am $datum beträgt Ihr Guthaben: $kontostand")
     }
 }
 
-fun shopkonto() {
-    println("am $datum beträgt Ihr Guthaben: $kontostand")
-}
+
+/*fun date() {
+    val now = LocalDate.now()
+//    val nowWithTime = LocalDateTime.now()
+//    println(nowWithTime)
+    println(now)
+}*/
+
+/*
+    val birthday = LocalDate.of(2000,1,1)
+    println(birthday)
+    val format = DateTimeFormatter.ofPattern("dd.MM.yyyy")*/
